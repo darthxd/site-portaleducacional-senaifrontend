af9c9176a26ce534a44f3ffedc335bff757cbfb9
darthxd <darthp87@gmail.com>
2024-12-04 13:41:16 -0300

    responsividade pt2.5

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
 const chkDarkMode = document.getElementById("chkDarkMode");
 
 const menu = document.getElementById("menu");
-const arrayContainer = document.querySelectorAll(".container");
 
 chkDarkMode.addEventListener('change', () => {
-    arrayContainer.forEach(container => {
-        container.classList.toggle("dark-mode");
-        for(const item of container.getElementsByTagName("*")){
-            item.classList.toggle("dark-mode")
-        }
-        if(container.classList.contains("dark-mode")) {
-            localStorage.setItem('dark-mode', 'enabled')
-        } else {
-            localStorage.setItem('dark-mode', 'disabled')
-        }
-    });
+    document.body.classList.toggle("dark-mode")
+    for(const item of document.body.getElementsByTagName("*")){
+        item.classList.toggle("dark-mode")
+    }
+    if(document.body.classList.contains("dark-mode")) {
+        localStorage.setItem('dark-mode', 'enabled')
+    } else {
+        localStorage.setItem('dark-mode', 'disabled')
+    }
 })
 
 if (localStorage.getItem('dark-mode') === 'enabled') {
-    arrayContainer.forEach(container => {
-        container.classList.add('dark-mode');
-        for(const item of container.getElementsByTagName("*")){
-            item.classList.toggle("dark-mode")
-        }
-    })
+    for(const item of document.body.getElementsByTagName("*")){
+        item.classList.add("dark-mode")
+    }
+    document.body.classList.add('dark-mode');
     chkDarkMode.checked = true; 
 }
